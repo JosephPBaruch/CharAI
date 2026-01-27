2665db7b037ebd98eef59b7896521e719c40acb0
Joseph Baruch <josephbaruch48@gmail.com>
2026-01-27 12:07:43 -0800

    testing new backend container image

--- backend/pipeline.sh
+++ backend/pipeline.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-IMAGE="char-ai-frontend"
+IMAGE="char-ai-backend"
 INTERNAL_PORT=8000
 EXTERNAL_PORT=8000
 
 docker build -t $IMAGE .
 
-docker run -d -p $EXTERNAL_PORT:$INTERNAL_PORT --name $IMAGE $IMAGE
-
+docker run -d --env-file .env -p $EXTERNAL_PORT:$INTERNAL_PORT --name $IMAGE $IMAGE
