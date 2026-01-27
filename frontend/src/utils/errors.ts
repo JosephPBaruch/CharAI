491c6729c69721a946c33c23170df386b217d869
josh <jnorlin@outlook.com>
2026-01-27 15:38:08 -0800

    feat: add backend error object parsing functionality

---
+++ frontend/src/utils/errors.ts
@@ -0,0 +1,17 @@
+import { type FieldErrors, type DjangoErrorResponse } from "../types/auth.ts";
+
+// Parses signup error response for field-specific errors, improving UX and simplifying frontend component code
+export function normalizeRegisterErrors(
+  error: DjangoErrorResponse,
+): FieldErrors {
+  const fieldErrors: FieldErrors = {};
+
+  for (const field in error) {
+    const messages = error[field];
+    if (!messages || messages.length === 0) continue;
+
+    fieldErrors[field] = messages[0];
+  }
+
+  return fieldErrors;
+}
