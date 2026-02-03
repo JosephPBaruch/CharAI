4171bd10dd771064383801100cdddb5545e9a9df
Joseph Baruch <josephbaruch48@gmail.com>
2026-02-03 14:50:03 -0800

    adding secret key

--- backend/pipeline.sh
+++ backend/pipeline.sh
@@ -8,6 +8,7 @@ EXTERNAL_PORT=8000
 docker build -t $IMAGE .
 
 docker run -d \
+    -e SECRET_KEY=supersecretkey \
     -e DATABASE_URL=postgresql://charai:testpass@postgres-test:5432/charai_test \
     -e OPENTOPOGRAPHY_API_KEY=keykey \
     -e ALLOWED_HOSTS=localhost,127.0.0.1,char-ai-frontend \
